6ad4aeb56bd8677a546ab51fc8fcdaec981619a9
Aleksey Sinyagin <aleksin@microsoft.com>
2017-11-26 08:28:53 -0800

    fixing npm references

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # DTML.GamesShell
-ReactJS Shell for educational games hosted on dtml.org
+ReactJS Shell for educational games hosted on dtml.org. 
+
+#Setup
+
+1. Download NodeJS and NPM: https://nodejs.org/en/
+2. run > npm install
+3. run > npm start
+
+#Demo
+You can see shell running here: http://blog.dtml.org/games/index.html
--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
-import './App.css';
 import Social from './social.js';
 import list from './gamelist.json';
-import {
-  ShareButtons,
-  ShareCounts,
-  generateShareIcon
-} from 'react-share';
-
 import Rater from 'react-rater'
-
-
-
+import './App.css';
 
 class App extends Component {
    componentDidMount() {
@@ -55,41 +46,41 @@ class Body extends Component {
   };
   
   toggleCode()
-  {
-        var domNode = this.refs.framecontainer;
+  {        
+    var domNode = this.refs.framecontainer;
 		var frameCode = domNode.innerHTML+"<div><a href='http://dtml.org'>Game provided by dtml.org</a></div>";
-	    this.setState({displayCode: true});
+    this.setState({displayCode: true});
 		this.setState({frameText: frameCode});		
   }
    
     updateState(index) {
     this.setState({displayCode: false}); 
-	this.setState({currentLesson: index});
+   	this.setState({currentLesson: index});
 	  this.props.list.map(function(item, i){item.active = false;});        
 	  this.props.list[index].active = true;	  
    };
    
   render() {
     return (
-<div class="bodyContainer">
-  <div class="row">
-   <div class="col-sm-2 col-md-2 nav">
-	<section id="content">
-		<h4>ESL Games</h4>
-		<LeftNav data={this.props.list} click={this.updateState}/>
-	</section>	
-	</div>
+      <div class="bodyContainer">
+      <div class="row">
+      <div class="col-sm-2 col-md-2 nav">
+	    <section id="content">
+	   	<h4>ESL Games</h4>
+		  <LeftNav data={this.props.list} click={this.updateState}/>
+     	</section>	
+	   </div>
 	    <div class="col-sm-6 col-md-8 main">
-	<section id="middle">
-		<h4>{this.props.list[this.state.currentLesson].title}</h4>
-		<p>{this.props.list[this.state.currentLesson].description}</p>
-		<Social />
-		<div id="framecontainer" ref="framecontainer">
-		<iframe class='gameframe' src={this.props.list[this.state.currentLesson].url} width='100%' height="600" frameborder='0'></iframe>
-		</div>
-		<div>
-		<span>Rate this game </span>
-		<Rater total={5} rating={2} />
+    	<section id="middle">
+		  <h4>{this.props.list[this.state.currentLesson].title}</h4>
+		  <p>{this.props.list[this.state.currentLesson].description}</p>
+		  <Social />
+		  <div id="framecontainer" ref="framecontainer">
+		  <iframe class='gameframe' src={this.props.list[this.state.currentLesson].url} width='100%' height="600" frameborder='0'></iframe>
+		  </div>
+		  <div>
+		  <span>Rate this game </span>
+	   	<Rater total={5} rating={2} />
 		</div>
 		
 		<button onClick={() => {this.toggleCode()}} type="button" class="btn btn-info getcode	">&lt;/&gt; Embed</button>
@@ -98,7 +89,7 @@ class Body extends Component {
 			: null
 			}
 			
-			<button onClick={() => {window.location.href='http://blog.dtml.org/contact'}} type="button" class="btn btn-info getcode	"> <svg aria-hidden="true" class="octicon octicon-mark-github" height="32" version="1.1" viewBox="0 0 16 16" width="32"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>Contribute</button>
+			<button onClick={() => {window.location.href='http://blog.dtml.org/contact'}} type="button" class="btn btn-info getcode	">Contribute</button>
 			
 	</section>
 	
